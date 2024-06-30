4bb8294f2314be02a68b85a05939858176dcfd3c
Tanzida Nowshin <nowshinsara555@gmail.com>
2024-06-30 20:48:17 +0600

    company and address added

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -1,10 +1,37 @@
-function play(){
-    hideElementById('start');
-    showElementById('aboutus');
-    
 
-}
-function  play2(){
-    hideElementById('aboutus');
-    showElementById('start')
-}
+    function hideElementById(id) {
+        document.getElementById(id).classList.add('hidden');
+    }
+
+    function showElementById(id) {
+        document.getElementById(id).classList.remove('hidden');
+    }
+
+    function play() {
+        hideElementById('start');
+        hideElementById('company');
+        hideElementById('address')
+        showElementById('aboutus');
+    }
+
+    function play2() {
+        hideElementById('aboutus');
+        hideElementById('company');
+        hideElementById('address')
+        showElementById('start');
+    }
+
+    function play3() {
+        hideElementById('start');
+        hideElementById('aboutus');
+        hideElementById('address')
+        showElementById('company');
+
+    }
+    function play4() {
+        hideElementById('start');
+        hideElementById('aboutus')
+        hideElementById('company');
+        showElementById('address')
+    }
+
